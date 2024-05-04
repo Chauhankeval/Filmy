2ad1e9e4bbb2e4b7349a4bab3c9f6e0e16e624b5
Chauhan keval <kevalachauhan2017@gmail.com>
2024-05-04 22:12:15 +0530

    add new signUp code

--- filmy-city/src/components/Login.js
+++ filmy-city/src/components/Login.js
@@ -2,15 +2,45 @@ import React, { useState } from 'react'
 import { TailSpin } from 'react-loader-spinner'
 import { Link } from 'react-router-dom'
 import Video2 from '../Video/bg-2.mp4'
-
+import { signInWithEmailAndPassword } from 'firebase/auth'
 const Login = () => {
-    const [Form, setFrom] = useState({
-        mobile: "",
-        password: ""
-    })
+
     const [loading, setLoading] = useState(false)
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
+    const login = async () => {
+        setLoading(true);
+        try {
+            const res = await signInWithEmailAndPassword(auth, email, password);
+            toast.success("successfully Login", {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+            localStorage.setItem("user", JSON.stringify(res));
+            navigate("/");
+        } catch (error) {
+            toast.error(error, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+            setLoading(false);
+        }
+        setLoading(false);
+    };
 
 
 
@@ -30,27 +60,27 @@ const Login = () => {
                     <div class="lg:w-3/6 md:w-1/2 bg-gray-900 bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
                         <h2 class="text-white text-lg font-medium title-font mb-5">Login</h2>
                         <div class="relative mb-4">
-                            <label for="full-name" class="leading-7 text-sm text-gray-400">Mobile No</label>
+                            <label for="full-name" class="leading-7 text-sm text-gray-400">Email</label>
                             <input type="text"
-                                typeof='number'
-                                value={Form.mobile}
-                                onChange={(e) => setFrom({ ...Form, mobile: e.target.value })}
+                                typeof='Email'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                                 id="full-name" name="full-name" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-orange-400 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                         </div>
 
                         <div class="relative mb-4">
                             <label for="full-name" class="leading-7 text-sm text-gray-400">Password</label>
                             <input type="password"
-                                value={Form.password}
-                                onChange={(e) => setFrom({ ...Form, password: e.target.value })}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
                                 id="full-name" name="full-name" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-orange-400 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                         </div>
 
                         <div class="p-2 w-full">
-                            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                            <button onClick={login} class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                                 {
-                                loading ? <TailSpin height={25} color='white' /> : "Submit"
-                            }</button>
+                                    loading ? <TailSpin height={25} color='white' /> : "Submit"
+                                }</button>
                         </div>
                         <p class="text-xs mt-3 ">don't have a Account?
                             <Link to={'/singup'}>
